366354da245203b34390b5a33c44628655d7a374
microacup <xiangmain@gmail.com>
2016-12-26 09:46:32 +0800

    聊天demo

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
     room
+
+    <button @click="newRoom">newRoom</button>
   </div>
 
 </template>
 
 <script>
-  const socket = require('socket.io-client')('ws://localhost:7000', { transports: ['websocket'] });
+  const socket = require('socket.io-client')('ws://localhost:7000/chatroom', { transports: ['websocket'] });
 
   export default {
     name: 'ChatRoom',
+    data() {
+      return {
+        roomId: '1001',
+      };
+    },
     created() {
       socket.on('connect', () => {
-        console.log('connect');
+        socket.emit('join', this.roomId);
+        console.log(`connect ${this.roomId}`);
+
+        socket.on('updateUsersList', (userId) => {
+          console.log(`${userId} 加入了房间`);
+        });
+        socket.on('addMessage', (message) => {
+          console.log(`new message ${message}`);
+        });
+        socket.on('userLeave', (userId) => {
+          console.log(`${userId} leaved`);
+        });
       });
+    },
+    methods: {
+      newRoom() {
+        socket.emit('newMessage', this.roomId, '哈哈哈');
+      },
     },
   };
 
